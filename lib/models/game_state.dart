7b70d470019e68225be49c33f6075e377c6e9c61
ZongwenSun <szongwen@gmail.com>
2022-05-27 18:37:09 +0800

    feat: rotate support wall kick

--- lib/models/game_state.dart
+++ lib/models/game_state.dart
@@ -64,9 +64,19 @@ class GameState extends ChangeNotifier {
   }
 
   void rotateLeft() {
-    if (currentMino != null) {
-      currentMino!.rotateLeft();
-      notifyListeners();
+    if (currentMino == null || currentMino!.type == MinoType.O) {
+      return;
+    }
+    List<KickOffset> kickOffsets = WallKickData.getLeftRotationWallKickOffsets(currentMino!.type, currentMino!.state);
+    for (var item in kickOffsets) {
+      Mino nextMino = currentMino!.clone()
+        ..moveDelta(item.dx, item.dy)
+        ..rotateLeft();
+      if (allowMino(nextMino)) {
+        currentMino = nextMino;
+        notifyListeners();
+        break;
+      }
     }
   }
 
@@ -150,6 +160,63 @@ class SquareImage {
   final int color;
 
   SquareImage(this.color);
+}
+
+class KickOffset {
+  final int dx;
+  final int dy;
+
+  KickOffset(this.dx, this.dy);
+}
+
+class WallKickData {
+  static List<List<KickOffset>> rightRotationForMiniI = [
+    [KickOffset(0, 0), KickOffset(-2, 0), KickOffset(1, 0), KickOffset(-2, -1), KickOffset(1, 2)], // up->right
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(2, 0), KickOffset(-1, 2), KickOffset(2, -1)], //right->down
+    [KickOffset(0, 0), KickOffset(2, 0), KickOffset(-1, 0), KickOffset(2, 1), KickOffset(-1, -2)], // down->left
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(-2, 0), KickOffset(1, -2), KickOffset(-2, 1)], //left->up
+  ];
+
+  static List<List<KickOffset>> leftRotationForMiniI = [
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(2, 0), KickOffset(-1, 2), KickOffset(2, -1)], // up->left
+    [KickOffset(0, 0), KickOffset(2, 0), KickOffset(-1, 0), KickOffset(2, 1), KickOffset(-1, -2)], // right->up
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(-2, 0), KickOffset(1, -2), KickOffset(-2, 1)], // down->right
+    [KickOffset(0, 0), KickOffset(-2, 0), KickOffset(1, 0), KickOffset(-2, -1), KickOffset(1, 2)], // left->down
+  ];
+
+  static List<List<KickOffset>> rightRotatioinForOtherMini = [
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(-1, 1), KickOffset(0, -2), KickOffset(-1, -2)], // up->right
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(1, -1), KickOffset(0, 2), KickOffset(1, 2)], //right->down
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(1, 1), KickOffset(0, -2), KickOffset(1, -2)], // down->left
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(-1, -1), KickOffset(0, 2), KickOffset(-1, 2)], //left->up
+  ];
+
+  static List<List<KickOffset>> leftRotatioinForOtherMini = [
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(1, 1), KickOffset(0, -2), KickOffset(1, -2)], // up->left
+    [KickOffset(0, 0), KickOffset(1, 0), KickOffset(1, -1), KickOffset(0, 2), KickOffset(1, 2)], // right->up
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(-1, 1), KickOffset(0, -2), KickOffset(-1, -2)], // down->right
+    [KickOffset(0, 0), KickOffset(-1, 0), KickOffset(-1, -1), KickOffset(0, 2), KickOffset(-1, 2)], // left->down
+  ];
+
+  static List<KickOffset> getRightRotationWallKickOffsets(MinoType type, MinoState state) {
+    if (type == MinoType.I) {
+      return rightRotationForMiniI[state.index];
+    } else if (type == MinoType.O) {
+      return [];
+    } else {
+      return rightRotatioinForOtherMini[state.index];
+    }
+  }
+
+  static List<KickOffset> getLeftRotationWallKickOffsets(MinoType type, MinoState state) {
+    if (type == MinoType.I) {
+      return leftRotationForMiniI[state.index];
+    } else if (type == MinoType.O) {
+      return [];
+    } else {
+      return leftRotatioinForOtherMini[state.index];
+    }
+  }
 }
 
 class Mino {
